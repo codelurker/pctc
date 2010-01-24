54f135238fa3a4a82b444cacb668e05c527de8e9
Josh Holland <jrh@joshh.co.uk>
2010-01-24 16:10:56 +0000

    Added docstrings to pctclib.ui
    Also changed the name of UI.wrapped_refresh to UI._wrapped_refresh as
    it's an internal-only method.

--- pctclib/ui.py
+++ pctclib/ui.py
@@ -1,16 +1,56 @@
 # pctc: python curses twitter client
+"""Control the UI for PCTC.
+
+Classes:
+    Tweet: represent a tweet as an urwid widget
+    UI:    run the UI
+"""
 
 import urwid
 import re
 
 class Tweet(urwid.Text):
+    """Represent a tweet.
+
+    Minimal implementation to make tweets selectable and pass keypresses up the
+    widget hierarchy.
+    """
     def selectable(self):
+        """Return True to allow selection."""
         return True
 
     def keypress(self, size, key):
+        """Do nothing.
+
+        Return the key to propagate it to a widget that can handle it.
+        """
         return key
 
 class UI(object):
+    """Control the UI for PCTC.
+
+    Public methods:
+        change_focus: switch the focus to the next area of the window
+        handle:       handle a keypress
+        post:         post a tweet
+        refresh:      check for new updates and replies
+        reply:        start replying to a tweet
+        scroll:       move to the top or bottom of a column on home or end
+
+    Class constants:
+        palette: colour settings for the UI
+
+    Instance variables:
+        self.columns:  urwid.Columns holding self.updates and self.replies
+        self.footer:   urwid.Edit for entering updates to post
+        self.focussed: variable to keep track of the currently focussed area
+        self.frame:    top level urwid.Frame widget
+        self.replies:  list of Tweet objects for replies
+        self.twitobj:  the pctclib.backend.twitter object passed to the
+                       constructor
+        self.updates:  list of Tweet objects for updates
+    """
+
     palette = [
             ('header', 'light green', 'dark blue'),
             ('footer', 'header'),
@@ -19,6 +59,7 @@ class UI(object):
         ]
 
     def __init__(self, twitobj):
+        """Start the PCTC UI running with the twitobj passed in."""
         self.twitobj = twitobj
 
         header = urwid.Text("Welcome to PCTC. Logged in as %s (@%s)." %
@@ -35,7 +76,7 @@ class UI(object):
         self.focussed = 'footer'
         wrapped = urwid.AttrMap(self.frame, 'bg')
         loop = urwid.MainLoop(wrapped, UI.palette, unhandled_input=self.handle)
-        loop.set_alarm_in(300, self.wrapped_refresh)
+        loop.set_alarm_in(300, self._wrapped_refresh)
         try:
             loop.run()
         except KeyboardInterrupt:
@@ -43,6 +84,11 @@ class UI(object):
             raise urwid.ExitMainLoop
 
     def _wrap_statuses(self, statuses):
+        """Prepare a list of strings for use in self.columns.
+
+        The strings are turned into Tweet objects, given an urwid.AttrMap to
+        colour them when focussed, then built into an urwid.ListBox.
+        """
         textlist = map(Tweet, statuses)
         walker = urwid.SimpleListWalker([
                 urwid.AttrMap(w, None, 'in focus') for w in textlist
@@ -50,6 +96,16 @@ class UI(object):
         return urwid.ListBox(walker)
 
     def handle(self, keys):
+        """Handle keypresses.
+
+        Current bindings:
+            tab:   change focus
+            enter: post a tweet
+            f5:    refresh the screen
+            home:  scroll the current column to the top
+            end:   scroll the current column to the bottom
+            r:     reply (see docs for self.reply)
+        """
         methdict = {
                 'tab'   : self.change_focus,
                 'enter' : self.post,
@@ -64,6 +120,7 @@ class UI(object):
             pass
 
     def change_focus(self):
+        """Switch focus between the footer and the body."""
         if self.focussed == 'footer':
             self.frame.set_focus('body')
             self.focussed = 'body'
@@ -72,6 +129,10 @@ class UI(object):
             self.focussed = 'footer'
 
     def post(self):
+        """Post the currently entered text to Twitter.
+
+        Silently fails if the text is too long.
+        """
         edit = self.footer.original_widget
         text = edit.get_edit_text()
         edit.set_edit_text('')
@@ -79,6 +140,7 @@ class UI(object):
             self.twitobj.post(text)
 
     def refresh(self):
+        """Update the updates and replies."""
         for category in ('replies', 'updates'):
             walker = getattr(self, category).body
             statuses = getattr(self.twitobj, 'get_%s' % category)()
@@ -87,6 +149,7 @@ class UI(object):
             walker[:] = statuses
 
     def scroll(self, key):
+        """Move the currently focussed column to the top or the bottom."""
         widget = self.columns.get_focus()
         if key == "home":
             widget.set_focus(0)
@@ -94,6 +157,11 @@ class UI(object):
             widget.set_focus(len(widget.body))
 
     def reply(self):
+        """Begin a reply to the currently selected tweet.
+
+        This method moves the focus from the columns to the footer and adds
+        @user_who_posted_selected_tweet to the beginning.
+        """
         listbox = self.columns.get_focus()
         tweet, pos = listbox.get_focus()
         tweettext = tweet.original_widget.text
@@ -103,6 +171,7 @@ class UI(object):
         edit.insert_text(' ')
         self.frame.set_focus('footer')
 
-    def wrapped_refresh(self, loop, *args):
+    def _wrapped_refresh(self, loop, *args):
+        """Refresh and set the loop to refresh again in 300 secs."""
         self.refresh()
-        loop.set_alarm_in(300, self.wrapped_refresh)
+        loop.set_alarm_in(300, self._wrapped_refresh)
