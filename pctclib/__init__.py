4793d0cc14e0fe797680990884fa358d9f796796
Josh Holland <jrh@joshh.co.uk>
2010-01-24 16:15:20 +0000

    Added docstrings to pctclib package

--- pctclib/__init__.py
+++ pctclib/__init__.py
@@ -1,1 +1,12 @@
+"""Library to do the heavy lifting for PCTC.
+
+Modules:
+    backend: contains the Twitter interface
+    config:  reads and manages config files
+    ui:      controls the UI
+
+Constants:
+    version: current version of PCTC as a tuple
+"""
+
 version = (0, 0, 1)
